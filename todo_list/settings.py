daf46bd6449ad45970aaca79b159137d84c6d9e4
nomad <Pardus92@yandex.ru>
2024-01-08 23:52:08 +0300

    добавлен общий пагинатор, сделана модель авторов(юзеров)

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles', 
     'corsheaders',   
     'authors',
-    'TODO'
+    'TODO',
+    # 'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -134,9 +135,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL='authors.User'
 
-# REST_FRAMEWORK ={
-#     'DEFAULT_RENDERER_CLASSES':{
-#         'djangorestframework_camel_case.render.CamelCaseJSONrenderer'
-#         'djangorestframework_camel_case.render.CamelCaseBrowsableAPIrenderer'
-#     }
-# }
+REST_FRAMEWORK ={
+    'DEFAULT_RENDERER_CLASSES': [
+    'rest_framework.renderers.JSONRenderer',
+    'rest_framework.renderers.BrowsableAPIRenderer',
+    ],
+    # 'DEFAULT_FILTER_BACKENDS':[
+    #     'django_fiters.rest_framework.DjangoFilterBackend'
+    # ],
+    'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100
+}
